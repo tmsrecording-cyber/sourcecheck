dbb1345a8e4166d4ead58c0e5d5b1ee2576a0e4b
tmsrecording-cyber <your-email@example.com>
2026-03-16 23:37:36 -0700

    Explicitly set outputFileTracingRoot and turbopack.root to same value

--- backend/next.config.mjs
+++ backend/next.config.mjs
@@ -1,4 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const root = path.join(__dirname);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  outputFileTracingRoot: root,
+  turbopack: {
+    root,
+  },
+};
 
 export default nextConfig;
